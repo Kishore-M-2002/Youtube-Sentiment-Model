693219e0ec917df112b85b754187fc5964289782
Kishore-M <135300028+Kishore-M-2002@users.noreply.github.com>
2026-05-07 13:42:58 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="youtube-sentiment-model",
+    version="0.1.0",
+    author="Kishore M",
+    packages = find_packages(),
+    install_requires=[]
+    
+)
